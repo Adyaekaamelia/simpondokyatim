04a8bd2a622974cc8759937bcecb6841bb28e86b
Adyaekaamelia <Adyka013@gmail.com>
2022-03-24 10:21:22 +0700

    apiawal

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\API\DataanakController;
+use App\Http\Controllers\API\KategoriController;
+use App\Http\Controllers\API\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,4 +23,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::resource('kegiatan', KategoriController::class);
+Route::resource('dataanak', DataanakController::class);
+
+Route::resource('User', UserController::class);
 Route::resource('dataanak2', ApiController::class);
